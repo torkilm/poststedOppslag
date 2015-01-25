f098b2e736fe9ecf0c5a232491f6be1b7984bc49
torkilm <torkil@heimdalit.no>
2015-01-25 22:23:47 +0100

    Opprettet
    Henter poststed fra Bring API

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = angular.module('myApp', ['ngResource']);
+
+app.controller('postCtl', ['$scope', 'postLookup', 
+  function ($scope, poststed) {  
+    $scope.Search = function () {
+      poststed.get({'pnr':$scope.postNr}, function (data) {
+        $scope.error = "";
+        if (data.valid) {
+          $scope.Resultat = data;
+          $scope.sted = data.result;
+          console.log(data);
+        } else {
+          console.log("Invalid!");
+          $scope.error = "Ugyldig postnummer!"
+        }
+        
+      }); // End function
+    }; //End Search 
+  }]); // End controller
+
+app.factory('postLookup', ['$resource', 
+  function ($resource){
+    return $resource('https://api.bring.com/shippingguide/api/postalCode.json?clientUrl=insertYourClientUrlHere&country=no');
+  }]); // End factory https://api.bring.com/shippingguide/api/postalCode.json?clientUrl=insertYourClientUrlHere&country=no&pnr=7056
